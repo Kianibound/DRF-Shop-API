cd6332080e808589fa80edfee3e0209bb17a4d82
kianibound <kianibound@gmail.com>
2025-10-21 14:25:03 +0330

    Created Models, config some settings, and populated database

--- api/models.py
+++ api/models.py
@@ -1,3 +1,4 @@
+import uuid
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
@@ -19,3 +20,35 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Order(models.Model):
+    class StatusChoices(models.TextChoices):
+        PENDING = 'pending'
+        CONFIRMED = 'confirmed'
+        CANCELLED = 'cancelled'
+
+    order_id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(
+        max_length=10, choices=StatusChoices.choices, default=StatusChoices.PENDING)
+
+    products = models.ManyToManyField(
+        Product, through='OrderItem', related_name='orders')
+
+    def __str__(self):
+        return f"Order {self.order_id} by {self.user.username}"
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+
+    @property
+    def item_subtotal(self):
+        return self.product.price * self.quantity
+      
+    def __str__(self):
+        return f"{self.quantity} x {self.product.name} in Order {self.order.order_id}"
