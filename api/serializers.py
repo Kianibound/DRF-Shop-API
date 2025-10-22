e086e2d1847e8df990cca3df43f1974c9ec18dac
kianibound <kianibound@gmail.com>
2025-10-22 12:47:10 +0330

    Add order management: enhance OrderItem model, implement Order and OrderItem serializers, and create order_list view

--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,7 @@
 from django.forms import fields
+from rest_framework.serializers import ModelSerializer
 from rest_framework import serializers
-from api.models import Product, Order, OrderItem
+from api.models import Order, OrderItem, Product
 
 
 class ProductSerializer(serializers.ModelSerializer):
@@ -14,8 +15,44 @@ class ProductSerializer(serializers.ModelSerializer):
             'stock',
         )
 
-        def valiadte_price(self, value):
+        def validate_price(self, value):
             if value <= 0:
                 raise serializers.ValidationError(
                     "Price must be greater than 0")
             return value
+
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    product_name = serializers.CharField(source='product.name', read_only=True)
+    product_price = serializers.DecimalField(
+        source='product.price', max_digits=10, decimal_places=2, read_only=True)
+
+    class Meta:
+        model = OrderItem
+        fields = (
+            'product_name',
+            'product_price',
+            'quantity',
+            'item_subtotal'
+        )
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    items = OrderItemSerializer(many=True, read_only=True)
+    total_price = serializers.SerializerMethodField(method_name='total')
+    # TODO : Make A guide for what is serializer Method Field and how to use it in DRF
+
+    def total(self, obj):
+        order_items = obj.items.all()
+        return sum(order_item.item_subtotal for order_item in order_items)
+
+    class Meta:
+        model = Order
+        fields = (
+            'order_id',
+            'user',
+            'created_at',
+            'status',
+            'items',
+            'total_price',
+        )
