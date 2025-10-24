427e1749fef57a74274327f1fe8c9aa5dc5fe461
kianibound <kianibound@gmail.com>
2025-10-24 13:00:40 +0330

    Add user order tests and enforce authentication for user orders endpoint

--- api/views.py
+++ api/views.py
@@ -4,6 +4,7 @@ from api.serializers import ProductSerializer, OrderSerializer, ProductInfoSeria
 from api.models import Product, Order, OrderItem
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework.permissions import IsAuthenticated
 from rest_framework import generics
 
 
@@ -26,6 +27,7 @@ class OrderListAPIView(generics.ListAPIView):
 class UserOrderListAPIView(generics.ListAPIView):
     queryset = Order.objects.prefetch_related('items__product')
     serializer_class = OrderSerializer
+    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         qs = super().get_queryset()
