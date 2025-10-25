49b41c2515eab70988131bd45080d62cbc2d0d97
kianibound <kianibound@gmail.com>
2025-10-25 13:57:37 +0330

    Add ProductFilter for enhanced product querying and update settings for filter backend

--- api/views.py
+++ api/views.py
@@ -6,11 +6,13 @@ from rest_framework.response import Response
 from rest_framework.permissions import AllowAny, IsAdminUser, IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework import generics
+from api.filters import ProductFilter
 
 
 class ProductListCreateAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    filterset_class = ProductFilter
 
     def get_permissions(self):
         self.permission_classes = [AllowAny]
